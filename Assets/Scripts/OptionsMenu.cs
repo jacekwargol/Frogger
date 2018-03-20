5df77dc39af2c26ad32dcb4f4d23fc28de4f5f6f
Ricenwind <jwargol@gmail.com>
2018-03-20 02:07:13 +0100

    Add timer

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour {
     [SerializeField] private AudioMixer audioMixer;
+
+    private Slider slider;
 
 
     public void SetVolume(float volume) {
---
+++ Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    [SerializeField] private float startingTime = 90f;
+
+    private float timeLeft;
+    private Slider timer;
+
+    void Start() {
+        timeLeft = startingTime;
+        timer = GetComponent<Slider>();
+        timer.maxValue = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft <= 0) {
+            GameManager.Instance.HandleLose();
+        }
+        else {
+            timer.value = timeLeft;
+        }
+    }
+}
